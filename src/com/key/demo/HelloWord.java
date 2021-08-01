e0ca0b91b823c0bcf9d372d887e012179d75edbd
KeyHuy <2848823130@qq.com>
2021-08-01 15:28:28 +0800

    HelloWord_Version01

---
+++ src/com/key/demo/HelloWord.java
@@ -0,0 +1,14 @@
+package com.key.demo;
+
+/**
+ * helloWord！
+ *  - git测试程序
+ *
+ * @author Key
+ * @date 2021/08/01/15:16
+ **/
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWord!");
+    }
+}
